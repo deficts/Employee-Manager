90dbdf31f26b2ad9d9cc4da03621081e654a6b9e
deficts <diegoar7@live.com>
2019-09-16 11:11:00 -0500

    configuring firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDkE3h9glN3qiyOauhtu8dSnr6F6PpFfMI",
+  authDomain: "vuefs-d8d5c.firebaseapp.com",
+  databaseURL: "https://vuefs-d8d5c.firebaseio.com",
+  projectId: "vuefs-d8d5c",
+  storageBucket: "vuefs-d8d5c.appspot.com",
+  messagingSenderId: "168372602244",
+  appId: "1:168372602244:web:b97086f74a1fbbb17b09d1"
+}
